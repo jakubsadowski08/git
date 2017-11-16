462dc0ca1928b0ebf71c164a25fb3046b75cc785
szuzanna <zsmiech@student.agh.edu.pl>
2017-11-16 14:51:30 +0100

    chinski_test_pierwszosci

---
+++ chinski_test.cpp
@@ -0,0 +1,38 @@
+// Sito Liniowe
+// Data   : 17.03.2008
+// (C)2012 mgr Jerzy Wa³aszek
+//----------------------------
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  unsigned int i,n,p,q,x;
+  bool * S;
+
+  cin >> n;
+  S = new bool[n + 1];
+  for(i = 2; i <= n; i++) S[i] = true;
+  p = 2;
+  while(p * p <= n)
+  {
+    q = p;
+    while(p * q <= n)
+    {
+      x = p * q;
+      while(x <= n)
+      {
+        S[x] = false;
+        x *= p;
+      }
+      while(!S[++q]);
+    }
+    while(!S[++p]);
+  }
+  for(i = 2; i <= n; i++) if(S[i]) cout << i << " ";
+  cout << endl;
+  delete [] S;
+  return 0;
+}
